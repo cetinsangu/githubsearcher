4072d9d8ffdca1b7c7148981f0da0b319d8a06f1
cetinsangu <sangucetin@gmail.com>
2022-07-19 22:35:02 +0300

    navbar login link redirecting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,12 +15,12 @@ function Navbar() {
             </span>
           </Link>
           <div className="flex items-center lg:order-2">
-            <a
-              href="#"
+            <Link
+              to={'/login'}
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
             >
               Log In
-            </a>
+            </Link>
             <button
               onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}
               type="button"
